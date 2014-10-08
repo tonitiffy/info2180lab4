68ec95bcd0196b29d5c36ff5a28d42dadbf7992e
tonitiffy <tonitiffy@users.noreply.github.com>
2014-10-08 10:41:44 -0500

    New

--- maze.js
+++ maze.js
@@ -1,34 +1,51 @@
-var loser = false;  // whether the user has hit a wall
+// CSE 190 M, solution to Mouse Maze lab
+
+var loser = null;  // whether the user has hit a wall
 
 window.onload = function() {
-    $("start").onclick = startClick;
-    $("end").onmouseover = overEnd;
+    $("start").onclick = Click;
+    $("end").onmouseover = End;
     var boundaries = $$("div#maze div.boundary");
     for (var i = 0; i < boundaries.length; i++) {
         boundaries[i].onmouseover = overBoundary;
     }
+    document.body.observe("mousemove", onBody);   // haxor exercise
 };
 
-function overBoundary() {
-    loser = true;
-    var boundaries = $$("div#maze div.boundary");
-    for (var i = 0; i < boundaries.length; i++) {
-        boundaries[i].addClassName("youlose");
+
+function onBody(event) {
+    if (loser === false && event.element() == document.body) {
+        overBoundary(event);
     }
 }
 
-function startClick() {
+
+function overBoundary(event) {
+    if (loser === false) {
+        loser = true;
+        $("status").textContent = "Haha you lose!";
+        var boundaries = $$("div#maze div.boundary");
+        for (var i = 0; i < boundaries.length; i++) {
+            boundaries[i].addClassName("youlose");
+        }
+        event.stop();  
+    }
+}
+
+
+function Click() {
     loser = false;
+    $("status").textContent = "Begin!";
     var boundaries = $$("div#maze div.boundary");
     for (var i = 0; i < boundaries.length; i++) {
         boundaries[i].removeClassName("youlose");
     }
 }
 
-function overEnd() {
-    if(loser) {
-        alert("Sorry, you lost. :[");
-    } else {
-        alert("You win! :]");
+
+function End() {
+    if (loser === false) {
+        $("status").textContent = "Your good at this you win";
     }
+    event.stop();
 }
